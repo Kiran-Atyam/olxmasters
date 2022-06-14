be005b39d0a1a6d54031cb10b6199334df5a0807
Kiran-Atyam <104185917+Kiran-Atyam@users.noreply.github.com>
2022-06-14 20:17:02 +0530

    Add files via upload

---
+++ 13-Olx-Masters/src/main/java/com/zensar/service/AdvertisementStatusService.java
@@ -0,0 +1,23 @@
+package com.zensar.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zensar.bean.AdvertisementStatus;
+import com.zensar.db.AdertisementStatusDAO;
+
+@Service
+public class AdvertisementStatusService {
+   @Autowired
+   AdertisementStatusDAO dao;
+   
+   public AdvertisementStatus addAdvertisementStatus(AdvertisementStatus advertisementStatus) {
+	   return this.dao.save(advertisementStatus);
+   }
+   
+   public List<AdvertisementStatus> getAllAdvertisementStatus(){
+	   return this.dao.findAll();
+   }
+}
